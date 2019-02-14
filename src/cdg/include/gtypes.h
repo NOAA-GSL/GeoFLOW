d88dee928ca0a40ff71ae2d9b233230edbd48776
Duane Rosenberg <duane.rosenberg@rosenburg-lt.local>
2019-02-13 21:35:31 -0700

    Add member function to geoflow namespace

--- src/cdg/include/gtypes.h
+++ src/cdg/include/gtypes.h
@@ -208,4 +208,24 @@ const char * const sGStepperType[] =    {
 extern std::stringstream oss_global_;
 #define GPP(comm,a)    oss_global_ << a << std::endl; GComm::cout(comm,oss_global_.str().c_str()); oss_global_.str(std::string());
 
+namespace geoflow {
+//**********************************************************************************
+//**********************************************************************************
+// METHOD : str2bdytype
+// DESC   : Convert string to GBdyType
+// ARGS   : stype: string type
+// RETURNS: GBdyType
+//**********************************************************************************
+GBdyType str2bdytype(const GString &stype)
+{
+  GString s0;
+  for ( auto j=0; j<GBDY_NONE; j++ ) {
+    s0 = sGBdyType[j];
+    if ( stype.compare(s0) == 0 ) return static_cast<GBdyType>(j);
+  }
+  assert(FALSE && "Invalid boundary type");
+} // end, str2bdytype
+
+}
+
 #endif // _G_TYPES_DEF
