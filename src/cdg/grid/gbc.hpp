59c159e73d5f000bc7206016b74e9719cc9fb030
Duane Rosenberg <duane.rosenberg@enki.fsl.noaa.gov>
2019-02-15 01:18:15 +0000

    Intermed. commit.

--- src/cdg/grid/gbc.hpp
+++ src/cdg/grid/gbc.hpp
@@ -24,9 +24,9 @@ public:
       void                   update_bdy_state(GFTYPE &t, GTVector<GTVector<GFTYPE>*> &u,        
                              GTVector<GTVector<GFTYPE>*> &ub);                 // entry function
       void                   set_dirichlet_callback(
-                             std::function<void(GFTYPE &t, GTVector<GTVector<GFTYPE>*> &u,
-                             GTVector<GTVector<GFTYPE>*> &ub)> *callback)
-                             {update_dirichlet_callback_ = callback; }         // set bdy-update callback
+                             std::function<void(const GFTYPE &t, GTVector<GTVector<GFTYPE>*> &u,
+                             GTVector<GTVector<GFTYPE>*> &ub)> &callback)
+                             {update_dirichlet_callback_ = &callback; }         // set bdy-update callback
 
 
 private:
@@ -35,7 +35,7 @@ private:
                               GTVector<GTVector<GFTYPE>*> &ub); 
 
         GGrid                  *grid_;                                // element list
-        std::function<void(GFTYPE &t, GTVector<GTVector<GFTYPE>*> &u, 
+        std::function<void(const GFTYPE &t, GTVector<GTVector<GFTYPE>*> &u, 
         GTVector<GTVector<GFTYPE>*> &ub)>
                                *update_dirichlet_callback_;           // dirichlet bdy update callback function
 
