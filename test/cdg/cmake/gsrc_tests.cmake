be0ab204ffd38abb911988bf8cc8f352810f734a
Duane Rosenberg <duane.rosenberg@enki.fsl.noaa.gov>
2019-03-09 00:10:03 +0000

    .

--- test/cdg/cmake/gsrc_tests.cmake
+++ test/cdg/cmake/gsrc_tests.cmake
@@ -7,9 +7,10 @@
 
  # Include only sources that contain main here:
  set(CDG_TEST_MAIN  
+#                   ${GTHOME}/gtest_rk.cpp
                     ${GTHOME}/gtest_burgers.cpp
-#                   ${GTHOME}/gtest_derivs.cpp
 #                   ${GTHOME}/gtest_helm.cpp
+#                   ${GTHOME}/gtest_derivs.cpp
 #                   ${GTHOME}/gtest_mass.cpp
 #                   ${GTHOME}/gtest_ggrid.cpp
 #                   ${GTHOME}/gtest_gmtk.cpp
