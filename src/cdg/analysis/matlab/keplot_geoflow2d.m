796a2ccabf883cd7f945a63f5229608f29762656
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2019-03-30 23:03:24 +0000

    Plot kinetic energy on 2d grids

---
+++ src/cdg/analysis/matlab/keplot_geoflow2d.m
@@ -0,0 +1,55 @@
+function h = keplot_geoglow2d(s1var, s2var, tindex)
+%
+% Does a mesh plot kinetic energy of 2D GeoFLOW data
+%
+
+ntasks = 2;
+scoord = {'xgrid','ygrid' 'zgrid'};
+
+d = dir('xgrid.*');
+ntasks = length(d);
+if ntasks<= 0 
+  error('Grid data missing or incomplete');
+end
+
+for itask = 0:ntasks-1
+
+  % Read node coords:
+  for j=1:2
+    fname = sprintf('%s.%04d.out', scoord{j}, itask)
+    [x{j} dim nelems porder gtype time] = rgeoflow(fname, 8, 'ieee-le');
+  end
+
+
+  fname = sprintf('%s.%06d.%04d.out', s1var, tindex, itask);
+  [u1 dim nelems porder gtype time] = rgeoflow(fname, 8, 'ieee-le');
+  fname = sprintf('%s.%06d.%04d.out', s2var, tindex, itask);
+  [u2 dim nelems porder gtype time] = rgeoflow(fname, 8, 'ieee-le');
+  if ( itask == 0 )
+    figure;
+  end
+
+  NN = double(porder + 1);
+  lelem = prod(NN);  % data length per element
+
+  % Cycle over elems, and plot 'patches':
+  icurr = 1;
+  for n = 1:nelems
+    xx = x{1}(icurr:icurr+lelem-1);
+    yy = x{2}(icurr:icurr+lelem-1);
+    uu1 = u1 (icurr:icurr+lelem-1);
+    uu2 = u2 (icurr:icurr+lelem-1);
+    xx = reshape(xx , NN(1), NN(2));
+    yy = reshape(yy , NN(1), NN(2));
+    uu1= reshape(uu1, NN(1), NN(2));
+    uu2= reshape(uu2, NN(1), NN(2));
+    surf( xx, yy, uu1.^2+uu2.^2 )
+    title(sprintf('%s t=%f', 'KE', time));
+    hold on
+    icurr = icurr + lelem ; 
+    
+  end % end, elem loop
+  
+
+end % end, task loop
+
