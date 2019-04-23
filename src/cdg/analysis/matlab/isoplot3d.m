3aa06f70eb68955f1e1e73453d9b305cb875df93
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2019-04-23 21:17:28 +0000

    First commit of 3d isoplot script for GIO data

---
+++ src/cdg/analysis/matlab/isoplot3d.m
@@ -0,0 +1,126 @@
+function h = isoplot3(svar, tindex, isoval)
+%
+% Does a isosurface plot of 2D GeoFLOW data
+%
+%  Usage:
+%    h  = isoplot3d('u1',10, 0.1)
+%
+%  Input:
+%    s1var   : prefix for field file. Required
+%    tindex  : time index for output. Required
+%    isoval  : isosurface value
+%
+%  Output:
+%    h       : plot handle
+
+%
+
+if nargin < 3
+  error('must specify svar, tindex, isoval');
+end 
+
+
+ntasks = 2;
+scoord = {'xgrid','ygrid' 'zgrid'};
+
+d = dir('xgrid.*');
+ntasks = length(d);
+if ntasks<= 0 
+  error('Grid data missing or incomplete');
+end
+
+for itask = 0:ntasks-1
+
+  % Read node coords:
+  for j=1:3
+    fname = sprintf('%s.%05d.out', scoord{j}, itask)
+    [x{j} dim nelems porder gtype icycle time] = rgeoflow(fname, 8, 'ieee-le');
+  end
+ 
+
+  fname = sprintf('%s.%06d.%05d.out', svar, tindex, itask);
+  [u dim nelems porder gtype icycle time] = rgeoflow(fname, 8, 'ieee-le');
+  if ( itask == 0 )
+    figure;
+  end
+
+ 
+  NN = double(porder + 1);
+  lelem = prod(NN(1:dim));  % data length per element
+
+  umax = 0.0;
+
+  % Cycle over elems, and plot 'patches':
+  icurr = 1;
+  for n = 1:nelems
+    xx = x{1}(icurr:icurr+lelem-1);
+    yy = x{2}(icurr:icurr+lelem-1);
+    zz = x{3}(icurr:icurr+lelem-1);
+    uu = u   (icurr:icurr+lelem-1);
+
+    xx   = reshape(xx, NN(1), NN(2), NN(3));
+    yy   = reshape(yy, NN(1), NN(2), NN(3));
+    zz   = reshape(zz, NN(1), NN(2), NN(3));
+    uu   = reshape(uu, NN(1), NN(2), NN(3));
+    dx   = diff(xx,1,1); dxx = min(abs(dx(:)));
+    dy   = diff(yy,1,2); dyy = min(abs(dy(:)));
+    dz   = diff(zz,1,3); dzz = min(abs(dz(:)));
+
+    xx   = reshape(xx, NN(1)* NN(2)* NN(3),1);
+    yy   = reshape(yy, NN(1)* NN(2)* NN(3),1);
+    zz   = reshape(zz, NN(1)* NN(2)* NN(3),1);
+    uu   = reshape(uu, NN(1)* NN(2)* NN(3),1);
+    pxyz = [xx yy zz];
+    V    = uu;
+    [pxyz, I] = unique(pxyz,'rows');
+    V         = uu(I);
+if 0
+    qx   = min(xx(:)) : 0.5*dxx : max(xx(:));
+    qy   = min(yy(:)) : 0.5*dyy : max(yy(:));
+    qz   = min(zz(:)) : 0.5*dzz : max(zz(:));
+    [xxi,yyi,zzi] = meshgrid(qx,qy,qz);
+else
+sprintf('sort...')
+    xm = unique(sort(xx(I))); 
+    ym = unique(sort(yy(I))); 
+    zm = unique(sort(zz(I)));
+sprintf('do meshgrid...')
+    [xxi,yyi,zzi] = meshgrid(xm,ym,zm);
+    pixyz = [xxi yyi zzi];
+sprintf('meshgrid done.')
+end
+if 0
+    F   = scatteredInterpolant(pxyz, V);
+%   F   = scatteredInterpolant(xx, yy, zz, uu);
+    Vi   = F(xxi,yyi,zzi);
+else
+sprintf('do interp...')
+    F   = TriScatteredInterp(pxyz, V, 'natural');
+%%  F   = TriScatteredInterp(xx, yy, zz, uu);
+    Vi  = F(xxi,yyi,zzi);
+%   Vi  = griddatan(pxyz, V, pixyz); 
+sprintf('interp done.')
+end
+sprintf('do isosurface...')
+    p = patch( isosurface( xxi, yyi, zzi, Vi, isoval ) );
+    isonormals(xxi, yyi, zzi, Vi, p);
+%   p.FaceColor = 'blue';
+%   p.EdgeColor = 'none';
+    set(p, 'FaceColor', 'blue', 'EdgeColor', 'none');
+    title(sprintf('%s t=%f: isoval=%f', svar, time, isoval));
+    hold on
+    icurr = icurr + lelem ; 
+
+    umax = max(umax, max(abs(uu(:))));
+    
+  end % end, elem loop
+  
+  umax
+
+end % end, task loop
+
+view(3)
+axis equal
+axis tight
+camlight
+lighting phong
