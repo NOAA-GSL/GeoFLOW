9a282f071774210388db6fa373d73abd73c08c91
Duane Rosenberg <duane.rosenberg@enki.fsl.noaa.gov>
2019-08-09 00:44:11 +0000

    First commit if system-defined bdy init, update, and specification methods; may be added to by user.

---
+++ src/cdg/init/gupdateb.hpp
@@ -0,0 +1,33 @@
+//==================================================================================
+// Module       : gupdateb.hpp
+// Date         : 7/11/19 (DLR)
+// Description  : Boundary update function implementations provided
+//                by system.
+// Copyright    : Copyright 2020. Colorado State University. All rights reserved.
+// Derived From : none.
+//==================================================================================
+#if !defined(_GUPDATEB_HPP)
+#define _GUPDATEB_HPP
+
+#include "tbox/property_tree.hpp"
+#include "gtypes.h"
+#include "gtvector.hpp"
+#include "ggrid.hpp"
+#include "gcomm.hpp"
+#include "gupdateb_user.hpp"
+
+
+using namespace geoflow;
+using namespace pdeint;
+
+namespace gupdateb
+{
+
+GBOOL impl_bystateinit          (const PropertyTree& stree, GGrid &grid,  Time &time, State &utmp, State &u, State &ub);
+GBOOL impl_simple_outflow       (const PropertyTree& stree, GGrid &grid,  Time &time, State &utmp, State &u, State &ub);
+GBOOL impl_noslip               (const PropertyTree& stree, GGrid &grid,  Time &time, State &utmp, State &u, State &ub);
+};
+
+#include "gupdateb.ipp"
+
+#endif
