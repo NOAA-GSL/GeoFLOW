85b041d212f61543aa1b2dbefc74016448a0bdc3
Duane Rosenberg <duane.rosenberg@rosenburg-lt.local>
2019-08-20 20:00:10 -0600

    First commit of user ginits for state

---
+++ src/cdg/user/state/ginits_user.hpp
@@ -0,0 +1,34 @@
+//==================================================================================
+// Module       : ginits_user.hpp
+// Date         : 7/10/19 (DLR)
+// Description  : User state initialization function implementations
+// Copyright    : Copyright 2020. Colorado State University. All rights reserved.
+// Derived From : none.
+//==================================================================================
+#if !defined(_GINITS_USER_HPP)
+#define _GINITS_USER_HPP
+
+#include "tbox/property_tree.hpp"
+#include "gtypes.h"
+#include "gtvector.hpp"
+#include "ggrid.hpp"
+#include "gcomm.hpp"
+
+using namespace geoflow;
+using namespace pdeint;
+
+typedef GFTYPE                      Time;
+typedef GTVector<GTVector<GFTYPE>*> State;
+
+namespace ginits
+{
+
+GBOOL impl_boxnwaveburgers      (const PropertyTree& stree, GGrid &grid,  Time &time, State &utmp, State &ub, State &u);
+GBOOL impl_boxdirgauss          (const PropertyTree& stree, GGrid &grid,  Time &time, State &utmp, State &ub, State &u);
+GBOOL impl_boxpergauss          (const PropertyTree& stree, GGrid &grid,  Time &time, State &utmp, State &ub, State &u);
+GBOOL impl_icosgauss            (const PropertyTree& stree, GGrid &grid,  Time &time, State &utmp, State &ub, State &u);
+};
+
+#include "ginits_burgers.ipp"
+
+#endif
