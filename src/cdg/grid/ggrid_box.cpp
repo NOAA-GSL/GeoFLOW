ab967bbf332eed61d7ec3f8901690ff59ead8d9c
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2020-07-20 18:52:18 +0000

    Ad ginitstate_factory to #include list

--- src/cdg/grid/ggrid_box.cpp
+++ src/cdg/grid/ggrid_box.cpp
@@ -14,6 +14,7 @@
 #include "ggrid_box.hpp"
 #include "gspecbdy_factory.hpp"
 #include "gupdatebdy_factory.hpp"
+#include "ginitstate_factory.hpp"
 #include "gmtk.hpp"
 #include "gutils.hpp"
 #include "tbox/mpixx.hpp"
