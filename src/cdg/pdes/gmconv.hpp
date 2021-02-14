321075afbcb8a3acd64a002dabfba9264354457f
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2021-02-14 01:45:36 +0000

    Correct number of vector components for different grids.

--- src/cdg/pdes/gmconv.hpp
+++ src/cdg/pdes/gmconv.hpp
@@ -229,6 +229,7 @@ inline  GINT                szrhstmp();
         GINT                nevolve_;       // num StateComp's evolved
         GINT                nhydro_;        // num hydrometeors
         GINT                nmoist_;        // number of moist components
+        GINT                nc_;            // number momentum components
         GSIZET              icycle_;        // internal cycle number
         GStepperType        isteptype_;     // stepper type
         GTVector<GFTYPE>    tcoeffs_;       // coeffs for time deriv
