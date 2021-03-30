3112134542b90168944c4beda6053298e57d2ac3
Duane Rosenberg <Duane.Rosenberg@noaa.gov>
2021-03-30 16:52:22 -0600

    Modify to allow SSP methods

--- test/sandbox/cdg/src/gtest_rk.cpp
+++ test/sandbox/cdg/src/gtest_rk.cpp
@@ -25,24 +25,27 @@ typedef GFTYPE Time;
 
 GFTYPE omega=1.0;
 
-void update_dirichlet(const Time &t, State &u, State &ub);
-void dudt(const Time &t, const State &u,
-          const Time &dt, State &dudt);
+void dudt(const Time &t, const State &u, const State &uf,
+          const State &ub, const Time &dt, State &dudt);
 
 
 int main(int argc, char **argv)
 {
 
     GString serr ="main: ";
+    GBOOL   bSSP = FALSE;
     GINT    errcode, iopt;
     GINT    nstate=GDIM;  // number 'state' arrays
-    GSIZET  nstage=2, maxSteps=1;
+    GSIZET  norder=2, nstage=2, maxSteps=1;
     GFTYPE  dt=1.0e-2, t, t0=0.0, tmax=3.14159, maxerror;
 
     // : option indicates that it takes an argument.
     // Note: -i reserved for InputManager:
-    while ((iopt = getopt(argc, argv, "d:t:n:w:h")) != -1) {
+    while ((iopt = getopt(argc, argv, "sd:t:n:o:w:h")) != -1) {
       switch (iopt) {
+      case 's': // set SSP flag
+          bSSP = TRUE;
+          break;
       case 'd': // set dt
           dt = atof(optarg);
           break;
@@ -52,12 +55,15 @@ int main(int argc, char **argv)
       case 'n': // set no. RK stages
           nstage = atoi(optarg);
           break;
+      case 'o': // set no. RK stages
+          norder = atoi(optarg);
+          break;
       case 'w': // set max timesteps
           omega = atof(optarg);
           break;
       case 'h': // help
           std::cout << "usage: " << std::endl <<
-          argv[0] << " [-h] [-d dt] [-t max time] [-n #stages] [-w omega]" << std::endl;
+          argv[0] << " [-h] [-d dt] [-t max time] [-n #stages] [-s] [-w omega]" << std::endl;
           exit(1); 
           break;
       case ':': // missing option argument
@@ -73,11 +79,18 @@ int main(int argc, char **argv)
     }
 
     // Create GExRK object:
-    GExRKStepper<GFTYPE> gexrk(nstage);
+    GExRKStepper<GFTYPE>::Traits rktraits;
 
-   std::function<void(const Time &t,                    // RHS callback function
+    rktraits.bSSP   = bSSP;
+    rktraits.norder = norder;
+    rktraits.nstage = nstage;
+    GExRKStepper<GFTYPE> gexrk(rktraits);
+
+    std::function<void(const Time   &t,              // RHS callback function
                        const State  &uin,
-                       const Time &dt,
+                       const State  &uf,
+                       const State  &ub,
+                       const Time   &dt,
                        State &dudt)> rhs = dudt;
 
     gexrk.setRHSfunction(rhs);
@@ -89,6 +102,7 @@ int main(int argc, char **argv)
     GTVector<GTVector<GFTYPE>*> uout(1);
     GTVector<GTVector<GFTYPE>*> ua  (1);
     GTVector<GTVector<GFTYPE>*> ub  (1);
+    GTVector<GTVector<GFTYPE>*> uf  (1);
     GTVector<GTVector<GFTYPE>*> utmp(u.size()*(nstage+2)+1);
     
     for ( GSIZET j=0; j<utmp.size(); j++ ) utmp[j] = new GTVector<GFTYPE>(1);
@@ -96,6 +110,7 @@ int main(int argc, char **argv)
     for ( GSIZET j=0; j<uout.size(); j++ ) uout[j] = new GTVector<GFTYPE>(1);
     for ( GSIZET j=0; j<ua  .size(); j++ ) ua  [j] = new GTVector<GFTYPE>(1);
     for ( GSIZET j=0; j<ub  .size(); j++ ) ub  [j] = new GTVector<GFTYPE>(1);
+    for ( GSIZET j=0; j<uf  .size(); j++ ) uf  [j] = new GTVector<GFTYPE>(1);
 
     // Find solution to 
     // du/dt = sin(omega t) at tmax: 
@@ -103,9 +118,10 @@ int main(int argc, char **argv)
     *u [0] = 0.0; // initialize numerical soln
 
     t = t0;
-    for ( GSIZET k=0; k<maxSteps; k++ ) {
-      gexrk.step(t, u, ub, dt, utmp);
-//    *u[0] = *uout[0];
+    for ( GSIZET k=0; k<maxSteps+1; k++ ) {
+      gexrk.step(t, u, uf, ub, dt, utmp, uout);
+      *u[0] = *uout[0];
+//    gexrk.step(t, u, uf, ub, dt, utmp);
       t += dt;
     }
 
@@ -115,7 +131,7 @@ int main(int argc, char **argv)
     cout << "main: relative error=" << maxerror << endl;
     cout << "main: maxSteps      =" << maxSteps << endl;
 
-    if ( maxerror > 10*std::numeric_limits<GFTYPE>::epsilon() ) {
+    if ( maxerror > 1000*std::numeric_limits<GFTYPE>::epsilon() ) {
       std::cout << "main: -------------------------------------RK stepper FAILED" << std::endl;
       errcode = 1;
     } else {
@@ -149,8 +165,8 @@ int main(int argc, char **argv)
 // DESC  : RHS function
 // ARGS  : 
 //**********************************************************************************
-void dudt(const Time &t, const State &u,
-          const Time &dt, State &dudt)
+void dudt(const Time &t, const State &u, const State &uf, 
+          const State &ub, const Time &dt, State &dudt)
 {
   (*dudt[0])[0] = sin(omega * t); 
 
