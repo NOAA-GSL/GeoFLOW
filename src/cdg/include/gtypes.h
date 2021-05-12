cc9cc161bd1c695901a28e93979f865dfb67141b
Duane Rosenberg <duane.rosenberg@rosenberg-lt.local>
2021-05-12 12:57:38 -0600

    Change GKEY size to size_t

--- src/cdg/include/gtypes.h
+++ src/cdg/include/gtypes.h
@@ -63,7 +63,7 @@ typedef std::string GString;
 #define GLLONG     GLONGLONG
 #define GWORD      int
 #define GDWORD     long
-#define GKEY       GUINT
+#define GKEY       size_t
 #define GNODEID    long long
 #define GSIZET     size_t
 #define GFPOS      size_t
