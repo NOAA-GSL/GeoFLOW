8ce01fbbb35edb245b9b07727204c6710b6fa08f
Bryan Flynt <72622421+BryanFlynt@users.noreply.github.com>
2021-06-03 16:40:58 -0600

    new tracer.cpp

--- src/tbox/tracer.cpp
+++ src/tbox/tracer.cpp
@@ -6,22 +6,22 @@
  */
 
 #include "tbox/tracer.hpp"
+#include "tbox/mpixx.hpp"
 
-#if defined( GEOFLOW_USE_TRACER )
+#if defined(GEOFLOW_USE_TRACER)
 
-#if defined( GEOFLOW_TRACER_USE_GPTL )
+#if defined(GEOFLOW_TRACER_USE_GPTL)
 #include "gptl.h"
+#include "gptlmpi.h"
 #endif
 
-#if defined( GEOFLOW_TRACER_USE_NVTX )
+#if defined(GEOFLOW_TRACER_USE_NVTX)
 #include "nvToolsExt.h"
 #endif
 
-#if defined( GEOFLOW_TRACER_USE_PIO )
+#if defined(GEOFLOW_TRACER_USE_PIO)
 #include "tbox/pio.hpp"
 #endif
-
-#include "boost/mpi.hpp"
 
 namespace geoflow {
 namespace tbox {
@@ -29,71 +29,67 @@ namespace tbox {
 // Initialize the number of times the Tracer has been called
 std::size_t Tracer::m_count = 0;
 
-Tracer::Tracer(const std::string message) : name_(message){
-#if defined( GEOFLOW_TRACER_USE_PIO )
-	std::string full_text = std::string(indent(),' ') + name_ + " -->";
-	pio::pout << full_text << std::endl;
-	indent() = indent() + m_nest_indent;
+Tracer::Tracer(const std::string message) : name_(message) {
+#if defined(GEOFLOW_TRACER_USE_PIO)
+    std::string full_text = std::string(indent(), ' ') + name_ + " -->";
+    pio::pout << full_text << std::endl;
+    indent() = indent() + m_nest_indent;
 #endif
-#if defined( GEOFLOW_TRACER_USE_NVTX )
-	m_count++;
-	 constexpr std::uint32_t colors[] = {0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff, 0xff00ffff, 0xffff0000, 0xffffffff};
-	 constexpr std::size_t num_colors = sizeof(colors)/sizeof(std::uint32_t);
-	 std::size_t color_id = m_count++ %  num_colors;
-	 nvtxEventAttributes_t eventAttrib = {0};
-	 eventAttrib.version       = NVTX_VERSION;
-     eventAttrib.size          = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
-     eventAttrib.colorType     = NVTX_COLOR_ARGB;
-	 eventAttrib.color         = colors[color_id];
-	 eventAttrib.messageType   = NVTX_MESSAGE_TYPE_ASCII;
-	 eventAttrib.message.ascii = name_.c_str();
-	 nvtxRangePushEx(&eventAttrib);
+#if defined(GEOFLOW_TRACER_USE_NVTX)
+    m_count++;
+    constexpr std::uint32_t colors[] = {0xff00ff00, 0xff0000ff, 0xffffff00, 0xffff00ff, 0xff00ffff, 0xffff0000, 0xffffffff};
+    constexpr std::size_t num_colors = sizeof(colors) / sizeof(std::uint32_t);
+    std::size_t color_id = m_count++ % num_colors;
+    nvtxEventAttributes_t eventAttrib = {0};
+    eventAttrib.version = NVTX_VERSION;
+    eventAttrib.size = NVTX_EVENT_ATTRIB_STRUCT_SIZE;
+    eventAttrib.colorType = NVTX_COLOR_ARGB;
+    eventAttrib.color = colors[color_id];
+    eventAttrib.messageType = NVTX_MESSAGE_TYPE_ASCII;
+    eventAttrib.message.ascii = name_.c_str();
+    nvtxRangePushEx(&eventAttrib);
 //	 nvtxRangePushA(message.c_str());
 #endif
-#if defined( GEOFLOW_TRACER_USE_GPTL )
-	 GPTLstart(name_.c_str());
+#if defined(GEOFLOW_TRACER_USE_GPTL)
+    GPTLstart(name_.c_str());
 #endif
 }
 
-Tracer::~Tracer(){
-#if defined( GEOFLOW_TRACER_USE_PIO )
-	indent() = indent() - m_nest_indent;
-	pio::pout << std::string(indent(),' ') << "<--" << std::endl;
+Tracer::~Tracer() {
+#if defined(GEOFLOW_TRACER_USE_PIO)
+    indent() = indent() - m_nest_indent;
+    pio::pout << std::string(indent(), ' ') << "<--" << std::endl;
 #endif
-#if defined( GEOFLOW_TRACER_USE_NVTX )
-	nvtxRangePop();
+#if defined(GEOFLOW_TRACER_USE_NVTX)
+    nvtxRangePop();
 #endif
-#if defined( GEOFLOW_TRACER_USE_GPTL )
-	 GPTLstop(name_.c_str());
-#endif
-}
-
-std::size_t& Tracer::indent(){
-	static std::size_t m_current_indent{0};
-	return m_current_indent;
-}
-
-
-
-
-void TracerOps::initialize(){
-#if defined( GEOFLOW_TRACER_USE_GPTL )
-	GPTLsetoption(GPTLcpu, 1);
-	GPTLsetoption(GPTLsync_mpi, 1);
-	GPTLinitialize();
+#if defined(GEOFLOW_TRACER_USE_GPTL)
+    GPTLstop(name_.c_str());
 #endif
 }
-void TracerOps::finalize(){
-#if defined( GEOFLOW_TRACER_USE_GPTL )
-	GPTLpr_file("timing.txt");
-    GPTLpr_summary();
+
+std::size_t& Tracer::indent() {
+    static std::size_t m_current_indent{0};
+    return m_current_indent;
+}
+
+void TracerOps::initialize() {
+#if defined(GEOFLOW_TRACER_USE_GPTL)
+    GPTLsetoption(GPTLcpu, 1);
+    GPTLsetoption(GPTLsync_mpi, 1);
+    GPTLinitialize();
+#endif
+}
+void TracerOps::finalize() {
+#if defined(GEOFLOW_TRACER_USE_GPTL)
+    using namespace ::geoflow::tbox;
+    GPTLpr_file("timing.txt");
+    GPTLpr_summary(mpixx::communicator());
     GPTLfinalize();
 #endif
 }
 
-} // namespace tbox
-} // namespace geoflow
+}  // namespace tbox
+}  // namespace geoflow
 
-
-
-#endif // defined( GEOFLOW_USE_TRACER )
+#endif  // defined( GEOFLOW_USE_TRACER )
